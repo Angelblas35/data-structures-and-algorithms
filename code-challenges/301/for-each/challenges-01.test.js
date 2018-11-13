ffb641ef808f175c2933bb21b6ddeff74e65289d
angel <angelblas17@gmail.com>
2018-11-13 11:01:37 -0800

    what I currently have

--- code-challenges/301/for-each/challenges-01.test.js
+++ code-challenges/301/for-each/challenges-01.test.js
@@ -8,12 +8,13 @@ Write a function named greeting that takes in a string and returns the string in
 Then, write a function named speaker that takes in a string and a callback function. The speaker function should return the string in all uppercase letters only by invoking the callback.
 ------------------------------------------------------------------------------------------------ */
 
-const greeting = (word) => {
-  // Solution code here...
+const greeting = word => {
+  word = word.toUpperCase();
+  return word;
 }
 
-const speaker = (message, callback) => {
-  // Solution code here...
+const speaker = (message,callback) => {
+  return message.callback(message);
 }
 
 /* ------------------------------------------------------------------------------------------------
